837d379920895bcc847857a55edfea4380d1f746
Gabrielrolonmatheus <129123616+Gabrielrolonmatheus@users.noreply.github.com>
2024-06-19 23:50:52 +0000

    19/06/24

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 const caixaPrincipal = document.querySelector(".caixa-principal");
-const caixaPerguntas= document.querySelector(".caixa-perguntas");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
-const perguntas =[
+const perguntas = [
   {
-    enunciado: "Qual é a cor dos olhos do Dayvidson?",
+    enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e aúdios hiper-realistas. Qual o seu primeiro pensamento?",
     alternativas: [
-      "Azuis",
-      "Verdes"
+      "Isso é assustador!",
+      "Isso é maravilhoso!"
     ]
   },
 
   {
-    enunciado: "Quem é o melhor vilão Jason ou Fred Krügger",
+    enunciado: "Com a descoberta desta tecnologia, chamada inteligência Artificial (IA), uma professora de tecnologia de uma escola decidiu fazer uma sequência de aulas sobre essa ferramenta. No fim de uma aula ela pede que você escreva um trabalho sobre o usa da IA em sala de aula. Qual atitude você toma?",
     alternativas: [
-      "Fred",
-      "Jason"
+      "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique em uma linguagem que facilite o entendimento.",
+      "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema."
     ]
   },
 
@@ -51,11 +51,12 @@ let perguntaAtual;
 
 function mostraAlternativas() {
   for (const alternativa of perguntaAtual.alternativas) {
-    const botaoAternativas = document.createElement("button");
+    const botaoAlternativas = document.createElement("button");
     botaoAlternativas.textContent = alternativa;
     caixaAlternativas.appendChild(botaoAlternativas);
   }
 }
+
 
 function mostraPergunta() {
   perguntaAtual = perguntas[atual];
